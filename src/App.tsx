8a60f9b573f4c8210edca141d3120b3f3823d5e0
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-05-31 10:16:32 -0300

    Criação do componente formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Botao from './components/botao';
+import Formulario from './components/formulario';
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
-      <Botao />
+      <Formulario />
     </div>
   );
 }
