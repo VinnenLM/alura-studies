865fd203fa40831bc15d5b3b0297228318c050fb
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-05-31 10:23:15 -0300

    Criação da função Lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Botao from './components/botao';
 import Formulario from './components/formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario />
+      <Lista />
     </div>
   );
 }
