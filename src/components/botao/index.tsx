8a34e6f41b2ff275cb382b23ece34002a5148e2a
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-01 09:24:20 -0300

    Modificação do botão para receber valores por parâmetro

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import botao from './style.module.scss';
 
-class Botao extends React.Component {
+class Botao extends React.Component<{ texto: string }> {
     render(): React.ReactNode {
         return (
-            <button className={botao.botao}>Botão</button>
+            <button className={botao.botao}>{this.props.texto}</button>
         )
     }
 }
