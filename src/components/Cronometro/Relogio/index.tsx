abbdf82ea7e3a8d00bf61f22de3e9814f29f89b2
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-02 14:22:12 -0300

    Criada função para pegar os valores da lista e adicionar no cronômetro

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,21 @@
 import estilo from './style.module.scss';
 
-function Relogio(){
+interface Props {
+    tempo: number | undefined
+}
+
+function Relogio({ tempo = 0 }: Props){
+    const minutos = Math.floor(tempo / 60);
+    const segundos = tempo % 60;
+    const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0');
+    const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, '0');
     return(
         <>
-            <span className={estilo.relogioNumero}>0</span>
-            <span className={estilo.relogioNumero}>0</span>
+            <span className={estilo.relogioNumero}>{minutoDezena}</span>
+            <span className={estilo.relogioNumero}>{minutoUnidade}</span>
             <span className={estilo.relogioDivisao}>:</span>
-            <span className={estilo.relogioNumero}>0</span>
-            <span className={estilo.relogioNumero}>0</span>
+            <span className={estilo.relogioNumero}>{segundoDezena}</span>
+            <span className={estilo.relogioNumero}>{segundoUnidade}</span>
         </>
     );
 }
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
+import tempoParaSegundos from "../../common/utils/date";
+import { ITarefa } from "../../types/tarefas";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import estilo from './style.module.scss';
 
-function Cronometro() {
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+function Cronometro({ selecionado }: Props) {
+
+    const [tempo, setTempo] = useState <number>();
+    
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    },[selecionado]);
+
     return (
         <div className={estilo.cronometro}>
             <p className={estilo.titulo}>Escolha um card e inicie o cronômetro</p>
             <div className={estilo.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo} />
             </div>
             <Botao
                 texto="Começar!"
