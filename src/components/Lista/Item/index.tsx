de30d766c25dc0918fa07ed14467448035264814
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2022-06-02 09:53:38 -0300

    Modificações para utilização do uuid

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { ITarefa } from "../../../types/tarefas";
 import lista from '../style.module.scss';
 
-function Item({tarefa, tempo}: {tarefa: string, tempo: string}) {
+function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+    console.log({tarefa, tempo, selecionado, completado, id});    
     return (
         <li className={lista.item}>
             <h3>{tarefa}</h3>
